f0efbfa9b8e9bc489e1165edd299ee1f48a77534
ltrucco <56170216+ltrucco@users.noreply.github.com>
2021-01-17 19:18:34 -0300

    Add footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,49 @@
 import React from 'react'
+import {FooterContainer, FooterWrap, FooterLinksContainer, FooterLinksWrapper, FooterLinkItems, FooterLinkTitle, FooterLink} from './FooterElements'
 
 const Footer = () => {
     return (
-        <>
-            
-        </>
+        <FooterContainer>
+            <FooterWrap>
+                <FooterLinksContainer>
+                    <FooterLinksWrapper>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>About Ss</FooterLinkTitle>
+                                <FooterLink to="/signin">How it works</FooterLink>
+                                <FooterLink to="/">Testimonials</FooterLink>
+                                <FooterLink to="/">Careers</FooterLink>
+                                <FooterLink to="/">Investors</FooterLink>
+                                <FooterLink to="/">Terms of Service</FooterLink>
+                        </FooterLinkItems>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>Videos</FooterLinkTitle>
+                                <FooterLink to="/">Submit video</FooterLink>
+                                <FooterLink to="/">Ambassadors</FooterLink>
+                                <FooterLink to="/">Agency</FooterLink>
+                                <FooterLink to="/">Influences</FooterLink>
+
+                        </FooterLinkItems>
+                    </FooterLinksWrapper>
+                    <FooterLinksWrapper>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>Contact Us</FooterLinkTitle>
+                                <FooterLink to="/">Contact</FooterLink>
+                                <FooterLink to="/">Support</FooterLink>
+                                <FooterLink to="/">Destination</FooterLink>
+                                <FooterLink to="/">Sponsorships</FooterLink>
+                                
+                        </FooterLinkItems>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>Social Media</FooterLinkTitle>
+                                <FooterLink to="/">Instagram</FooterLink>
+                                <FooterLink to="/">Facebook</FooterLink>
+                                <FooterLink to="/">Twitter</FooterLink>
+                                <FooterLink to="/">Youtube</FooterLink>
+                        </FooterLinkItems>
+                    </FooterLinksWrapper>
+                </FooterLinksContainer>
+            </FooterWrap>
+        </FooterContainer>
     )
 }
 
