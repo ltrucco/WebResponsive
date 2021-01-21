c2678e6ed3c73bbdb6372a0e28ea5591ca6a97fc
ltrucco <56170216+ltrucco@users.noreply.github.com>
2021-01-21 20:25:03 -0300

    Stlye fix

--- src/components/Services/ServicesElements.js
+++ src/components/Services/ServicesElements.js
@@ -50,7 +50,7 @@ export const ServicesCard = styled.div`
     transition: all 0.2s ease-in-out;
 
     &:hover{
-        transform: scale()(1.02);
+        transform: scale(1.02);
         transition: all 0.2s ease-in-out;
         cursor: pointer;
     }
