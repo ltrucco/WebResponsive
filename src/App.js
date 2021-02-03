145568d31b3f6078ea8ada10d317fcda4b22d912
ltrucco <56170216+ltrucco@users.noreply.github.com>
2021-02-02 22:02:46 -0300

    test build

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import SigninPage from './pages/signin';
 const App = () => {
   return (
     <div >
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <Switch>
           <Route path="/" component={Home} exact />
           <Route path="/signin" component={SigninPage} exact />
